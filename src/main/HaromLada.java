bdde6fd4937efd081cdd1c1221f640a0e401749e
vikierdos <csicsorka@gmail.com>
2024-09-11 14:17:19 +0200

    feladat létrehozva

---
+++ src/main/HaromLada.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ErdősViktória(SZF_20
+ */
+public class HaromLada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
